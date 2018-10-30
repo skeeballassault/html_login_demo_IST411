a670dc6264a31e8230a207a16c60ff648655d782
firewallach <dylanwallach80@gmail.com>
2018-10-30 18:59:57 -0400

    Added Icon

--- app.js
+++ app.js
@@ -3,8 +3,10 @@ var bodyParser = require('body-parser');
 var app = express()
 var port = process.env.PORT || 3000
 var path = require('path')
+var favicon = require('serve-favicon');
 
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(favicon(path.join(__dirname,'public','images','scroll.ico')));
 app.use(bodyParser.urlencoded({extended:true}));
 app.engine('html', require('ejs').renderFile);
 
